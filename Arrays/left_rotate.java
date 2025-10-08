633800d07de35f4aa6ed7006f525cc3810f312e4
Raghav1378 <raghavramani2004@gmail.com>
2025-10-08 21:39:15 +0530

    Resolve

--- Arrays/left_rotate.java
+++ Arrays/left_rotate.java
@@ -4,14 +4,20 @@ public class left_rotate {
         //First thing that i would be doing here is that i would create a temporary array that would be the size of the same as the number of the times we have to rotate it 
 
         int temp[] = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n; i++) {
             temp[i - 1] = arr[i];
         }
-
+        temp[n-1]=arr[0];
+        for (int i = 0; i < n; i++) {
+            System.out.println(temp[i]);
+        }
     }
 
     public static void main(String[] args) {
+        int n = 5;
 
+        int arr[] = { 1, 2, 3, 4, 5 };
+        rotate_array(arr, n);
     }
 
 }
